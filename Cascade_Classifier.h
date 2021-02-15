a557308459056c98d2473b70f57bd9a0720cb25d
Göksu Uzuntürk <79089774+goksu-uzunturk@users.noreply.github.com>
2021-02-15 16:46:37 +0300

    Update Cascade_Classifier.h

--- Cascade_Classifier.h
+++ Cascade_Classifier.h
@@ -18,7 +18,7 @@ public:
 	Cascade_Classifier(string path1, string path2); //constructer
 
 	tuple<int, int*, int*> Detecting_Faces(string image_path); // The function detects the faces by cascade classifier and returns the center coordinates of detected faces  
-															  // The function saves the resulting images to writing_path
-															  // 1st function call saves the resulting image under the name of Testing Image 1, 2nd function call saves the resulting image under thr name of Testing Image 2
+								   // The function saves the resulting images to writing_path
+								  // 1st function call saves the resulting image under the name of Testing Image 1, 2nd function call saves the resulting image under thr name of Testing Image 2
 
 };
